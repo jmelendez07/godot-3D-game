60c054425474c1aca215657c93b8d9319aae3eca
jmelendez07 <josemelendezramos123@gmail.com>
2025-12-05 11:04:18 -0500

    Add: Tutorial del juego terminado

--- player.gd
+++ player.gd
@@ -1,7 +1,11 @@
 extends CharacterBody3D
 
+signal hit
+
 @export var speed: float = 14.0
 @export var fall_acceleration = 75
+@export var jump_impulse = 20
+@export var bounce_impulse = 16
 
 var target_velocity = Vector3.ZERO
 
@@ -20,6 +24,9 @@ func _physics_process(delta):
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 		$Pivot.basis = Basis.looking_at(direction)
+		$AnimationPlayer.speed_scale = 4
+	else:
+		$AnimationPlayer.speed_scale = 1
 		
 	target_velocity.x = direction.x * speed
 	target_velocity.z = direction.z * speed
@@ -28,4 +35,28 @@ func _physics_process(delta):
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
 		
 	velocity = target_velocity
+	
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		target_velocity.y = jump_impulse 
+		
+	for index in range(get_slide_collision_count()):
+		var collision = get_slide_collision(index)
+		if collision.get_collider() == null:
+			continue
+			
+		if collision.get_collider().is_in_group("mob"):
+			var mob = collision.get_collider()
+			if Vector3.UP.dot(collision.get_normal()) > 0.1:
+				mob.squash()
+				target_velocity.y = bounce_impulse
+				break
+	
 	move_and_slide()
+	$Pivot.rotation.x = PI / 6 * velocity.y / jump_impulse
+
+func die():
+	hit.emit()
+	queue_free()
+
+func _on_mob_detector_body_entered(body: Node3D) -> void:
+	die()
